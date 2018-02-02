fa0bf09eaa0320694d8d668b79e13f44a7045397
lbandlish <lbandlish@gmail.com>
2018-02-02 23:20:53 +0530

    dspa q2 5 test cases

--- dspa2/dspa2.2.cpp
+++ dspa2/dspa2.2.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
-storeinput (vector < pair<int, int> > &v, int n);
+void storeinput (vector < pair<int, int> > &v, int n);
 void inorder(vector < pair<int, int> > &v, int n, int curr, int parent);
 
 
@@ -27,19 +28,23 @@ main()
 		
 		printf("\n");
 	}
-
-
 }
 
-storeinput (vector < pair<int, int> > &v, int n)
+int compare (const void * a, const void * b)
+{
+  return ( *(int*)a - *(int*)b );
+}
+
+void storeinput (vector < pair<int, int> > &v, int n)
 {
 	int n1, n2;
 
 	for (int i = 0; i < n-1; i++)
 	{
-		scanf("%d, %d", n1, n2);
-		v(i) = make_pair(n1, n2);
-
+		scanf("%d %d", &n1, &n2);
+		v[i] = make_pair(n1, n2);
+		// printf("%d\n", n1);
+		// printf("%d\n", n2);	
 	}
 
 }
@@ -52,14 +57,14 @@ void inorder(vector < pair<int, int> > &v, int n, int curr, int parent)
 	{
 		for (int i = 0; i < n-1; i++)
 		{
-			if (v(i).first == 1)
+			if (v[i].first == 1)
 			{
-				vcurr.push_back(v(i).second);
+				vcurr.push_back(v[i].second);
 			}
 
-			else if (v(i).second == 1)
+			else if (v[i].second == 1)
 			{
-				vcurr.push_back(v(i).first);
+				vcurr.push_back(v[i].first);
 			}
 		}
 	}
@@ -69,33 +74,35 @@ void inorder(vector < pair<int, int> > &v, int n, int curr, int parent)
 	{
 		for (int i = 0; i < n-1; i++)
 		{
-			if (v(i).first == curr)
+			if (v[i].first == curr)
 			{
-				if (v(i).second != parent)
+				if (v[i].second != parent)
 				{
-					vcurr.push_back(v(i).second);
+					vcurr.push_back(v[i].second);
 				}
 			}
 
-			if (v(i).second == curr)
+			if (v[i].second == curr)
 			{
-				if (v(i).first != parent)
+				if (v[i].first != parent)
 				{
-					vcurr.push_back(v(i).first);
+					vcurr.push_back(v[i].first);
 				}
 			}
 		}
 	}
 
+	qsort (vcurr.data(), vcurr.size(), sizeof(int), compare);
+
 	for ( int i = 0; i < (vcurr.size() / 2); i++ )
 	{
-		inorder(v, n, vcurr(i), curr);
+		inorder(v, n, vcurr[i], curr);
 	}
 
 	printf("%d ", curr);
 
 	for (int i = (vcurr.size()/2); i < vcurr.size(); i++)
 	{
-		inorder(v, n, vcurr(i), curr);
+		inorder(v, n, vcurr[i], curr);
 	}
 }
